c0f16076b6c673828a770ff49e9524f2bd809b83
Quentin Plessis <quentin.plessis@mo-t.com>
2022-02-22 10:45:31 +0900

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+fish_already_setup=0
+if command -v fish &> /dev/null
+then
+  fish_already_setup=1
+fi
+
+# Install dependencies via Bundle
+echo "Install dependencies via Bundle"
+brew bundle
+
+# Init fish and Oh My Fish
+if [ "$fish_already_setup" = '0' ]; then
+  # Set fish as default shell
+  echo "Ensures fish is default shell"
+  echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+  chsh -s /usr/local/bin/fish
+
+  # Install Oh My Fish
+  echo "Install Oh My Fish"
+  curl -L https://get.oh-my.fish | fish
+fi
+
+# Tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Neovim
+curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Anyenv
+echo "Init anyenv"
+anyenv install --init
+
+anyenv install goenv # Golang
+anyenv install pyenv # Python
+anyenv install rbenv # Ruby
+anyenv install tfenv # Terraform
+anyenv install nodenv # Node
+
+# TODO: package.json
+npm install -g graphql-language-service-cli
+npm install -g bash-language-server
+npm install -g vscode-langservers-extracted
+npm install -g sql-language-server
+npm install -g yaml-language-server
+npm install -g diagnostic-languageserver
+npm install --global yarn
+npm install -g wscat
